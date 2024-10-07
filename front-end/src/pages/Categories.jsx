e5d1d6cddd105db92c3d989bd07c29cffbe4751a
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-10-08 00:14:04 +0530

    functionality_category

--- front-end/src/pages/Categories.jsx
+++ front-end/src/pages/Categories.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { GifState } from '../context/gifContext';
 const Categories = () => {
+  const [results, setResults] = useState([]);
+  const { category } = useParams();
+  const { gif } = GifState
+
+  const fetchResults = async () => {
+    try {
+      const { data } = await gif.gifs(category, category);
+
+      setResults(data); 
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, [filter]);
   return (
     <div>
-    <h1>Categories</h1>      
+
     </div>
   )
 }
